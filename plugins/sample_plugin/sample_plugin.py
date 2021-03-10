2850c16a9f32eda5540ab71a7d088168018005e5
Marcus Boyd <mwb@geosol.com.au>
2021-03-10 13:22:59 +1030

    Import cadquery as cq
    instead of from cadquery import *

--- plugins/sample_plugin/sample_plugin.py
+++ plugins/sample_plugin/sample_plugin.py
@@ -1,10 +1,10 @@
-from cadquery import *
+import cadquery as cq
 
 def make_cubes(self, length):
     # self refers to the CQ or Workplane object
 
     # create the solid
-    s = Solid.makeBox(length, length, length, Vector(0, 0, 0))
+    s = cq.Solid.makeBox(length, length, length, cq.Vector(0, 0, 0))
 
-    # use CQ utility method to iterate over the stack an position the cubes
+    # use CQ utility method to iterate over the stack and position the cubes
     return self.eachpoint(lambda loc: s.located(loc), True)
