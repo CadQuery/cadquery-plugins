41f9b86ff72408b4f733777af471917eb85b6760
Jojain <romain.ferru@gmail.com>
2021-03-19 16:15:40 +0100

    infinite cylinder + cylinder selector working

--- plugins/more_selectors/more_selectors.py
+++ plugins/more_selectors/more_selectors.py
@@ -17,11 +17,11 @@ def make_cylinder(plane, radius, height = None):
     infinite = False
     if height is None:
         infinite = True
-        height = 5000
+        height = 10000
 
-    cyl = cq.Workplane(plane).circle(radius).extrude(height,both = infite)
+    cyl = cq.Workplane(plane).circle(radius).extrude(height,both = infinite)
     try:
-        show_object(cyl, options={"alpha":0.7, "color": (64, 164, 223)})
+        show_object(cyl, name = "selected cylinder", options={"alpha":0.7, "color": (64, 164, 223)})
     except NameError:
         pass
 
@@ -80,7 +80,7 @@ class InfiniteCylinderSelector(cq.Selector):
             projected_p = self.base.toLocalCoords(p)
             p_radius = sqrt(projected_p.x**2 + projected_p.y**2)
 
-            if p_radius < self.r and (projected_p.z < self.height and projected_p.z > 0):   
+            if p_radius < self.r:   
                 result.append(o)
                     
 
@@ -92,46 +92,10 @@ class CylinderSelector(InfiniteCylinderSelector):
 
     """
     def __init__(self, origin, along_axis, height, radius, debug=False):
-
-        self.r = radius
-        self.axis = self.get_axis(along_axis)
+        super().__init__(origin,along_axis, radius)
         self.height = height
-        xdir = self.get_ortho_vector(self.axis)
-        self.base = cq.Plane(cq.Vector(origin), xdir, self.axis.toTuple())
         if debug:
-            make_cylinder(self.base, height, radius)
-
-    def get_axis(self, axis_value):
-        named_vectors = {
-            "X" : cq.Vector(1,0,0),
-            "-X" : cq.Vector(-1,0,0),
-            "Y" : cq.Vector(0,1,0),
-            "-Y" : cq.Vector(0,-1,0),
-            "Z" : cq.Vector(0,0,1),
-            "-Z" : cq.Vector(0,0,-1)
-        }
-        if isinstance(axis_value, str):
-            try:
-                return named_vectors[axis_value]
-            except KeyError:
-                raise ValueError("Supported names are {}".format(list(named_vectors.keys())))    
-        else:
-            if cq.Vector(axis_value).Length != 0:
-                return cq.Vector(axis_value)
-            else:
-                raise ValueError("Axis of the cylinder must be non null")
-
-    def get_ortho_vector(self, vector):
-        #returns a vector orthogonal to the provided vector
-        if 0 not in vector.toTuple():
-            return (1,1, (-vector.x - vector.y)/vector.z)    
-        else:
-            if vector.x != 0:
-                return ((-vector.y - vector.z)/vector.x, 1, 1)
-            elif vector.y != 0:
-                return (1, (-vector.x - vector.z)/vector.y,1)
-            else:
-                return (1,1, (-vector.x - vector.y)/vector.z)
+            make_cylinder(self.base, radius, height)
 
     def filter(self, objectList):
         result =[]
@@ -141,13 +105,13 @@ class CylinderSelector(InfiniteCylinderSelector):
             p_radius = sqrt(projected_p.x**2 + projected_p.y**2)
 
             if p_radius < self.r and (projected_p.z < self.height and projected_p.z > 0):   
-                result.append(o)
-                    
+                result.append(o)                   
 
         return result
 
 # cyl = make_cylinder(cq.Plane((0,10,0), (1,0,0), (0,1,1)), 5, 4)
-select = cq.Workplane().box(10,10,10).edges(CylinderSelector((3,3,2),(0,0,1), 6, 6))
+select = cq.Workplane().box(10,10,10).edges(CylinderSelector((3,3,2),(0,0,1), 6, 6, debug=True))
+select = cq.Workplane().box(10,10,10).edges(InfiniteCylinderSelector((3,3,2), "X", 6, debug=True))
 # deb(cq.Workplane().box(10,10,10))
 show(select, name = "selec")
 
