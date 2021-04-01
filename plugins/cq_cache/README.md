3bc6e3895a6a910ee4a718a56dcd99dfb1fc88f4
Jojain <romain.ferru@gmail.com>
2021-04-01 23:23:04 +0200

    working version of the cq_cache

---
+++ plugins/cq_cache/README.md
@@ -0,0 +1,39 @@
+# more_selectors
+
+This plugin provide a decorator function that allow you to add file based cache to speed up the execution of your computational heavy cadquery functions.
+
+## Installation
+
+To install this plugin, the following line should be used.
+
+```
+pip install -e "git+https://github.com/CadQuery/cadquery-plugins.git#egg=cq_cache&subdirectory=plugins/cq_cache"
+```
+You can also clone the repository of the plugin and run in the repository the following command :
+```
+python setup.py install
+```
+
+## Dependencies
+
+This plugin has no dependencies other than the cadquery library.
+
+## Usage
+
+To use this plugin after it has been installed, just import it and use the decorator on your functions
+
+```python
+#decorate your functions that build computational heavy shapes
+import cq_cache
+
+@cq_cache(cache_size=1)
+def make_cube(a,b,c):
+    cube = cq.Workplane().box(a,b,c)
+    return cube
+
+for i in range(200):
+    make_cube(1,1,1+i)
+
+clear_cq_cache()
+#>>> Cache cleared for 1.036 MB
+```
--- plugins/cq_cache/cq_cache.py
+++ plugins/cq_cache/cq_cache.py
@@ -1,4 +1,5 @@
 import cadquery as cq 
+from cadquery import exporters, importers
 from functools import wraps
 import tempfile
 import os
@@ -8,31 +9,68 @@ CACHE_DIR_NAME = "cadquery_geom_cache"
 CACHE_DIR_PATH = os.path.join(TEMPDIR_PATH,CACHE_DIR_NAME)
 
 if CACHE_DIR_NAME not in os.listdir(TEMPDIR_PATH):
-    os.mkdir(CACHE_DIR_NAME)
- 
+    os.mkdir(CACHE_DIR_PATH)
 
-def un_decorateur_passant_un_argument(fonction_a_decorer):
- 
-    def un_wrapper_acceptant_des_arguments(arg1, arg2):
-        print("J'ai des arguments regarde :", arg1, arg2)
-        fonction_a_decorer(arg1, arg2)
- 
-    return un_wrapper_acceptant_des_arguments
 
-def cq_cache(memory = 100):
- 
-    def _cq_cache(cq_function):        
-        @wraps(cq_function) 
+def get_cache_dir_size(cache_dir_path):
+    total_size = 0
+    for dirpath, dirnames, filenames in os.walk(cache_dir_path):
+        for f in filenames:
+            fp = os.path.join(dirpath, f)
+            total_size += os.path.getsize(fp)
+    return total_size
 
+def delete_oldest_file(cache_dir_path):
+    cwd = os.getcwd()
+    os.chdir(cache_dir_path)
+    files = sorted(os.listdir(os.getcwd()), key=os.path.getmtime)
+    oldest = files[0]
+    os.remove(os.path.join(cache_dir_path,oldest))
+    os.chdir(cwd)
+
+
+def build_file_name(fct, *args, **kwargs):
+    SPACER = "_"
+    file_name = fct.__name__
+    for arg in args:
+        file_name += SPACER + str(arg)
+    for kwarg_value in kwargs.values():
+        file_name += SPACER + str(kwarg_value)
+
+    return file_name + ".step"
+
+def cq_cache(cache_size = 100):
+    """
+    Maximum cache memory in MB
+    """
+    def _cq_cache(function):     
+
+        @wraps(function) 
         def wrapper(*args, **kwargs):
-            if existe_deja:
-                return function_qui_charge_le_fichier
+            file_name = build_file_name(function, *args, **kwargs)
+
+            if file_name in os.listdir(CACHE_DIR_PATH):
+                return importers.importStep(os.path.join(CACHE_DIR_PATH,file_name))
             else:
-                return cq_function(*args, **kwargs)
- 
-        print("En tant que décorateur, je retourne le wrapper")
- 
+                shape = function(*args, **kwargs)
+                exporters.export(shape, os.path.join(CACHE_DIR_PATH,file_name))
+                
+                cache_dir_size = get_cache_dir_size(CACHE_DIR_PATH)
+                while (cache_dir_size/1024**2) > cache_size:
+                    delete_oldest_file(CACHE_DIR_PATH)
+                    cache_dir_size = get_cache_dir_size(CACHE_DIR_PATH)
+
+                return shape
+
         return wrapper
  
-    print("En tant que créateur de décorateur, je retourne un décorateur")
-    return mon_decorateur
+    return _cq_cache
+
+
+
+def clear_cq_cache():
+    cache_size = get_cache_dir_size(CACHE_DIR_PATH)
+    for cache_file in os.listdir(CACHE_DIR_PATH):
+        os.remove(os.path.join(CACHE_DIR_PATH, cache_file))
+    print(f"Cache cleared for {round(cache_size*1e-6,3)} MB ")
+
