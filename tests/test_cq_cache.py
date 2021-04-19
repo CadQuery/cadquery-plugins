68eee00c2f6e18af5dd931853fca45117cdc678a
Marcus Boyd <mwb@geosol.com.au>
2021-04-19 17:01:56 +0930

    black

--- tests/test_cq_cache.py
+++ tests/test_cq_cache.py
@@ -6,58 +6,65 @@ import os
 
 TEMPDIR_PATH = tempfile.gettempdir()
 CACHE_DIR_NAME = "cadquery_geom_cache"
-CACHE_DIR_PATH = os.path.join(TEMPDIR_PATH,CACHE_DIR_NAME)
+CACHE_DIR_PATH = os.path.join(TEMPDIR_PATH, CACHE_DIR_NAME)
 CACHE_SIZE = 0.1
 for f in os.listdir(CACHE_DIR_PATH):
-    os.remove(os.path.join(CACHE_DIR_PATH,f))
+    os.remove(os.path.join(CACHE_DIR_PATH, f))
+
 
 @cq_cache(CACHE_SIZE)
-def cube(a,b,c):
-    cube = cq.Workplane().box(a,b,c)
+def cube(a, b, c):
+    cube = cq.Workplane().box(a, b, c)
     return cube.val()
 
+
 def test_get_cache_dir_size():
-    with open(os.path.join(CACHE_DIR_PATH,"fill.txt"), "w") as f:
-        f.write("test") 
+    with open(os.path.join(CACHE_DIR_PATH, "fill.txt"), "w") as f:
+        f.write("test")
     assert get_cache_dir_size(CACHE_DIR_PATH) == 4
 
+
 def test_clear_cache():
-    with open(os.path.join(CACHE_DIR_PATH,"fill.txt"), "w") as f:
+    with open(os.path.join(CACHE_DIR_PATH, "fill.txt"), "w") as f:
         f.write("test")
     assert len(os.listdir(CACHE_DIR_PATH)) == 1
     clear_cq_cache()
     assert len(os.listdir(CACHE_DIR_PATH)) == 0
 
+
 def test_cache_file_creation():
     clear_cq_cache()
-    cube1 = cube(1,1,1)
-    cube2 = cube(1,1,1)
+    cube1 = cube(1, 1, 1)
+    cube2 = cube(1, 1, 1)
     files = os.listdir(CACHE_DIR_PATH)
     assert len(files) == 2
     assert "Y3ViZV8xXzFfMQ==.brep" in files
     assert "Y3ViZV8xXzFfMQ==" in files
 
+
 def test_not_exceeding_size():
     clear_cq_cache()
     for i in range(20):
-        cube(1,1,1+i)
-    assert get_cache_dir_size(CACHE_DIR_PATH) < CACHE_SIZE*1e6
+        cube(1, 1, 1 + i)
+    assert get_cache_dir_size(CACHE_DIR_PATH) < CACHE_SIZE * 1e6
+
 
 def test_cache_type_return():
-    cube1 = cube(1,1,1) #at first call get the type directly from function call
-    cube2 = cube(1,1,1) #at second call the decorator retrives the right type with some logic that may fail
-    assert isinstance(cube1,cadquery.occ_impl.shapes.Solid)
-    assert isinstance(cube2,cadquery.occ_impl.shapes.Solid)
+    cube1 = cube(1, 1, 1)  # at first call get the type directly from function call
+    # at second call the decorator retrives the right type with some logic that may fail
+    cube2 = cube(1, 1, 1)
+    assert isinstance(cube1, cadquery.occ_impl.shapes.Solid)
+    assert isinstance(cube2, cadquery.occ_impl.shapes.Solid)
+
 
 def test_cache_type_return_with_modified_function():
-
     @cq_cache(CACHE_SIZE)
-    def cube(a,b,c):
-        cube = cq.Workplane().box(a,b,c)
+    def cube(a, b, c):
+        cube = cq.Workplane().box(a, b, c)
         return cube
 
-    cube1 = cube(1,1,1) #at first call get the type directly from function call
-    cube2 = cube(1,1,1) #at second call the decorator retrives the right type with some logic that may fail
-    assert isinstance(cube1,cq.Workplane)
-    assert isinstance(cube2,cq.Workplane)
-
+    cube1 = cube(1, 1, 1)  # at first call get the type directly from function call
+    # at second call the decorator retrives the right type with some logic that may fail
+    cube2 = cube(1, 1, 1)
+    assert isinstance(cube1, cq.Workplane)
+    assert isinstance(cube2, cq.Workplane)
