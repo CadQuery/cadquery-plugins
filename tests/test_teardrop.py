1a1bc342751558bf438bf0069e5376aa10560490
Lorenz Neureuter <hello@lorenz.space>
2021-08-17 18:07:16 -0400

    Reformatted test with black.

--- tests/test_teardrop.py
+++ tests/test_teardrop.py
@@ -1,6 +1,6 @@
 import pytest
 import cadquery as cq
-from plugins.teardrop import teardrop 
+from plugins.teardrop import teardrop
 
 
 @pytest.fixture
@@ -8,7 +8,7 @@ def box():
     return cq.Workplane().box(8, 20, 20)
 
 
-def test_teardrop_makes_a_hole(box): 
+def test_teardrop_makes_a_hole(box):
     box_with_hole = box.faces(">X").workplane().teardrop(4).cutThruAll()
     assert box.faces().size() < box_with_hole.faces().size()
 
@@ -18,19 +18,25 @@ def test_teardrop_rotation():
     r = cq.Workplane("XZ").teardrop(rad, 90,).extrude(5)
     assert r.size() == 1
 
-    #expect vertex of shape pointing in -X
+    # expect vertex of shape pointing in -X
     x1 = r.vertices("<X").val().Center().x
-    assert x1 == pytest.approx(-5.65685, rel=1e-3) 
+    assert x1 == pytest.approx(-5.65685, rel=1e-3)
 
     r = cq.Workplane("XZ").teardrop(rad, -90,).extrude(5)
     x2 = r.vertices(">X").val().Center().x
-    assert abs(x1 + x2) < 0.001 
+    assert abs(x1 + x2) < 0.001
+
 
 def test_teardrop_clip():
     r = cq.Workplane("XZ").teardrop(4, 0, 4.1).extrude(5)
     assert r.vertices(">Z").val().Center().z == pytest.approx(4.1, rel=1e-3)
 
-@pytest.mark.parametrize("rad, clipval, reason", [(4, 4*2, "less"), (4, -4, "greater")]) 
+
+@pytest.mark.parametrize(
+    "rad, clipval, reason", [(4, 4 * 2, "less"), (4, -4, "greater")]
+)
 def test_teardrop_clip_value_illegal(box, rad, clipval, reason):
     with pytest.raises(ValueError, match=fr".* argument must be {reason} than .*"):
-        box_with_hole = box.faces(">X").workplane().teardrop(rad,0,clipval).cutThruAll()
+        box_with_hole = (
+            box.faces(">X").workplane().teardrop(rad, 0, clipval).cutThruAll()
+        )
