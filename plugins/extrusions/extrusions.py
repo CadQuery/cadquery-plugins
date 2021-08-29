9556093f81d0062fb04e78ed864f9838cb95fbe6
Martin Budden <mjbudden@gmail.com>
2021-08-29 15:45:49 +0100

    Updates after black and pylint.

--- plugins/extrusions/extrusions.py
+++ plugins/extrusions/extrusions.py
@@ -1,10 +1,10 @@
+from typing import TypeVar, Union, Tuple
+import importlib.resources
 import cadquery as cq
 from cadquery.occ_impl import importers
 from cadquery.occ_impl.geom import Vector, Location
 from cadquery.occ_impl.shapes import Face, sortWiresByBuildOrder
 import ezdxf
-import importlib.resources
-from typing import TypeVar, Union, Tuple
 
 T = TypeVar("T", bound="Workplane")
 
@@ -40,16 +40,28 @@ def _importDXFstream(stream, tol=1e-6, exclude=[]):
 def _extrusion(
     size: Tuple[float, float],
     profile,
-    len: float,
+    length: float,
     centered: Union[bool, Tuple[bool, bool, bool]],
 ):
+    """
+    Return an extrusion with the specified profile and length
+
+    :param size: size of the extrusion, used for centering
+    :param profile: filename of the extrusion's profile
+    :param length: length of the extrusion
+    :type length: float > 0
+    :param centered: If True, the extrusion will be centered around the reference point.
+        If False, the corner of the extrusion will be on the reference point and it will
+        extend in the positive x, y and z directions. Can also use a 3-tuple to
+        specify centering along each axis.
+    """
     ref = (
         importlib.resources.files("plugins.extrusions")
         .joinpath("profiles/")
         .joinpath(profile)
     )
-    with ref.open() as fp:
-        faces = _importDXFstream(fp)
+    with ref.open() as stream:
+        faces = _importDXFstream(stream)
 
     PROFILE = cq.Workplane("XY").newObject(faces).wires()
 
@@ -62,35 +74,105 @@ def _extrusion(
     if not centered[1]:
         offset += Vector(0, size[1] / 2, 0)
     if centered[2]:
-        offset += Vector(0, 0, -len / 2)
+        offset += Vector(0, 0, -length / 2)
 
-    e = PROFILE.toPending()._extrude(len).move(Location(offset))
-    return cq.Workplane("XY").newObject([e])
-
-
-def e2020(len: float, centered: Union[bool, Tuple[bool, bool, bool]] = True):
-    return _extrusion([20, 20], "e2020.dxf", len, centered)
+    extrusion = PROFILE.toPending()._extrude(length).move(Location(offset))
+    return cq.Workplane("XY").newObject([extrusion])
 
 
-def e2040(len: float, centered: Union[bool, Tuple[bool, bool, bool]] = True):
-    return _extrusion([40, 20], "e2040.dxf", len, centered)
+def e2020(length: float, centered: Union[bool, Tuple[bool, bool, bool]] = True):
+    """
+    Return a 2020 extrusion with the specified length
+
+    :param length: length of the extrusion
+    :type length: float > 0
+    :param centered: If True, the extrusion will be centered around the reference point.
+        If False, the corner of the extrusion will be on the reference point and it will
+        extend in the positive x, y and z directions. Can also use a 3-tuple to
+        specify centering along each axis.
+    """
+    return _extrusion([20, 20], "e2020.dxf", length, centered)
 
 
-def e2080(len: float, centered: Union[bool, Tuple[bool, bool, bool]] = True):
-    return _extrusion([80, 20], "e2080.dxf", len, centered)
+def e2040(length: float, centered: Union[bool, Tuple[bool, bool, bool]] = True):
+    """
+    Return a 2040 extrusion with the specified length
+
+    :param length: length of the extrusion
+    :type length: float > 0
+    :param centered: If True, the extrusion will be centered around the reference point.
+        If False, the corner of the extrusion will be on the reference point and it will
+        extend in the positive x, y and z directions. Can also use a 3-tuple to
+        specify centering along each axis.
+    """
+    return _extrusion([40, 20], "e2040.dxf", length, centered)
 
 
-def e4040(len: float, centered: Union[bool, Tuple[bool, bool, bool]] = True):
-    return _extrusion([40, 40], "e4040.dxf", len, centered)
+def e2080(length: float, centered: Union[bool, Tuple[bool, bool, bool]] = True):
+    """
+    Return a 2080 extrusion with the specified length
+
+    :param length: length of the extrusion
+    :type length: float > 0
+    :param centered: If True, the extrusion will be centered around the reference point.
+        If False, the corner of the extrusion will be on the reference point and it will
+        extend in the positive x, y and z directions. Can also use a 3-tuple to
+        specify centering along each axis.
+    """
+    return _extrusion([80, 20], "e2080.dxf", length, centered)
 
 
-def v2020(len: float, centered: Union[bool, Tuple[bool, bool, bool]] = True):
-    return _extrusion([20, 20], "v2020.dxf", len, centered)
+def e4040(length: float, centered: Union[bool, Tuple[bool, bool, bool]] = True):
+    """
+    Return a 4040 extrusion with the specified length
+
+    :param length: length of the extrusion
+    :type length: float > 0
+    :param centered: If True, the extrusion will be centered around the reference point.
+        If False, the corner of the extrusion will be on the reference point and it will
+        extend in the positive x, y and z directions. Can also use a 3-tuple to
+        specify centering along each axis.
+    """
+    return _extrusion([40, 40], "e4040.dxf", length, centered)
 
 
-def v2040(len: float, centered: Union[bool, Tuple[bool, bool, bool]] = True):
-    return _extrusion([40, 20], "v2040.dxf", len, centered)
+def v2020(length: float, centered: Union[bool, Tuple[bool, bool, bool]] = True):
+    """
+    Return a 2020 v-slot extrusion with the specified length
+
+    :param length: length of the extrusion
+    :type length: float > 0
+    :param centered: If True, the extrusion will be centered around the reference point.
+        If False, the corner of the extrusion will be on the reference point and it will
+        extend in the positive x, y and z directions. Can also use a 3-tuple to
+        specify centering along each axis.
+    """
+    return _extrusion([20, 20], "v2020.dxf", length, centered)
 
 
-def v4040(len: float, centered: Union[bool, Tuple[bool, bool, bool]] = True):
-    return _extrusion([40, 40], "v4040.dxf", len, centered)
+def v2040(length: float, centered: Union[bool, Tuple[bool, bool, bool]] = True):
+    """
+    Return a 2040 v-slot extrusion with the specified length
+
+    :param length: length of the extrusion
+    :type length: float > 0
+    :param centered: If True, the extrusion will be centered around the reference point.
+        If False, the corner of the extrusion will be on the reference point and it will
+        extend in the positive x, y and z directions. Can also use a 3-tuple to
+        specify centering along each axis.
+    """
+    return _extrusion([40, 20], "v2040.dxf", length, centered)
+
+
+def v4040(length: float, centered: Union[bool, Tuple[bool, bool, bool]] = True):
+    """
+    Return a 4040 v-slot extrusion with the specified length
+
+    :param length: length of the extrusion
+    :type length: float > 0
+    :param centered: If True, the extrusion will be centered around the reference point.
+        If False, the corner of the extrusion will be on the reference point and it will
+        extend in the positive x, y and z directions. Can also use a 3-tuple to
+        specify centering along each axis.
+    """
+    return _extrusion([40, 40], "v4040.dxf", length, centered)
