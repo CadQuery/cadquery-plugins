1f28331f63b0e39b3a3ef10681616acea4bbae7b
Bruno Agostini <bruno.agostini@gmail.com>
2022-02-14 20:49:54 +0100

    Add files via upload

---
+++ tests/test_fragment.py
@@ -0,0 +1,10 @@
+import cadquery as cq
+import plugins.fragment.fragment as fragment
+
+def test_fragment_volume():
+
+    plate = cq.Workplane('XY').box(20,20,20)
+    plate2 = cq.Workplane('XY').box(10,10,10).translate((10,10,10))
+    plate = plate.fragment(plate2)
+
+    assert (abs(plate.vals()[0].Solids()[1].Volume()) - 125.0) < 1e-6
