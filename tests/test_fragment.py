647331e310f6531ca1163f8a8e68367ba72ef87e
Bruno Agostini <bruno.agostini@gmail.com>
2022-02-14 22:16:57 +0100

    Update test_fragment.py

--- tests/test_fragment.py
+++ tests/test_fragment.py
@@ -3,8 +3,8 @@ import plugins.fragment.fragment as fragment
 
 def test_fragment_volume():
 
-    plate = cq.Workplane('XY').box(20,20,20)
-    plate2 = cq.Workplane('XY').box(10,10,10).translate((10,10,10))
+    plate = cq.Workplane("XY").box(20,20,20)
+    plate2 = cq.Workplane("XY").box(10,10,10).translate((10,10,10))
     plate = plate.fragment(plate2)
 
     assert (abs(plate.vals()[0].Solids()[1].Volume()) - 125.0) < 1e-6
