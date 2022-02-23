0b698cedf5fc58c04ee2e94cc913ba89e61eb68a
Bruno Agostini <bruno.agostini@gmail.com>
2022-02-23 16:08:13 +0100

    Add files via upload
    Number of shapes and volume of each of Shapes in Compund created by fragment operation should be checked in the existing test to make sure fragment really does what it says on the tin
    glue=True option should be exercised
    Each variant allowed by type hint (Optional[Union["Workplane", Solid, Compound]]) of toFragment argument should be tested. Maybe without extensive checks of number of shapes and volume of each Shape but at least to be sure that it works at all.

--- tests/test_fragment.py
+++ tests/test_fragment.py
@@ -7,5 +7,20 @@ def test_fragment_volume():
     plate = cq.Workplane("XY").box(20, 20, 20)
     plate2 = cq.Workplane("XY").box(10, 10, 10).translate((10, 10, 10))
     plate = plate.fragment(plate2)
+    plate_glue = plate.fragment(plate2, glue=True)
+    plate_tol = plate.fragment(plate2, tol=1e-6)
+    plate_solid = plate.fragment(plate2.val())
 
-    assert (abs(plate.vals()[0].Solids()[1].Volume()) - 125.0) < 1e-6
+    plate3 = cq.Workplane("XY").box(2, 2, 2).add(plate2)
+    vals = [o.vals()[0] for o in plate3.all()] 
+    plate3  = cq.Compound.makeCompound(vals)
+    plate_compound = plate.fragment(plate3)
+    
+    assert len(plate_solid.vals()[0].Solids())==3
+    assert len(plate_compound.vals()[0].Solids())==4
+    assert len(plate_tol.vals()[0].Solids())==3
+    assert len(plate_glue.vals()[0].Solids())==3
+    assert len(plate.vals()[0].Solids())==3
+    assert (abs(plate.vals()[0].Solids()[0].Volume()) - 7875.) < 1e-6
+    assert (abs(plate.vals()[0].Solids()[1].Volume()) - 125.) < 1e-6   
+    assert (abs(plate.vals()[0].Solids()[2].Volume()) - 875.) < 1e-6
